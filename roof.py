9e065c1e16fbf11bafc15f141aae962b7bcd20c3
pinhasran <46615280+pinhasran@users.noreply.github.com>
2019-01-12 05:17:19 +0200

    Delete roof.py

--- roof.py
+++
@@ -1,7 +0,0 @@
-def roof(z,dy):
-    numerator=[]
-    denominator=[]
-    for i in range(len(z)):
-        numerator.append(z[i]/(dy[i])**2)
-        denominator.append(1/(dy[i])**2)
-    return (sum(numerator)/sum(denominator))

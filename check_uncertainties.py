c52095e54ee295faacaa571510631f6b9ccc10ab
pinhasran <46615280+pinhasran@users.noreply.github.com>
2019-01-12 05:16:49 +0200

    Delete check_uncertainties.py

--- check_uncertainties.py
+++
@@ -1,8 +0,0 @@
-def check_uncertainties(values_dict): #check if valid
-    for i in values_dict['dx']:
-        if i<0:
-            return "Input file error: Not all uncertainties are positive."
-    for i in values_dict['dy']:
-        if i<0:
-            return "Input file error: Not all uncertainties are positive."
-    return 0
